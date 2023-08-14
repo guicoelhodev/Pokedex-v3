aae339472bfc70e8e807f4dc4320f22e1ecfe163
guicoelhodev <gs.coelho@outlook.com>
2023-08-13 22:47:38 -0300

    feat: add QueryProvider to load react query hooks

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,21 +1,31 @@
+import styled from "styled-components";
 import { GlobalStyle } from "../components/style/globalStyle";
 import { PokemonData } from "../context/usePokemonData";
 import HomePage from "../pages/Home";
 import Pokedex from "../pages/Pokedex";
 import Pokemon from "../pages/Pokemon";
 
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient();
+
+const EmptyDiv = styled.div`
+  width: 100%;
+  height: 300px;
+`;
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <GlobalStyle />
       <HomePage />
+      <EmptyDiv />
 
       <PokemonData>
         <Pokemon />
       </PokemonData>
 
       <Pokedex />
-    </>
+    </QueryClientProvider>
   );
 }
 
