c9d77f089c18b8d08074ba69f12c7227806b20e8
guicoelhodev <gs.coelho@outlook.com>
2023-08-13 22:47:38 -0300

    refactor: Pokemon info component
    Remove query logic from component to react query hook

--- src/pages/Pokemon/index.tsx
+++ src/pages/Pokemon/index.tsx
@@ -1,135 +1,40 @@
 import * as S from "./style";
 import SvgWave from "../../components/SvgElement";
-import SvgWaveSmartphone from "../../components/SvgPhone";
-import useWindowDimensions from "../../hook/useWindowDimensions";
 import SearchPokemon from "../../components/SearchPokemon";
-import { useContext, useEffect, useState } from "react";
-import { PokemonContext } from "../../context/usePokemonData";
-import api from "../../service/api";
+import { useState } from "react";
 import { getColorPokemon } from "../../utils/getColorPokemon";
-import PokemonContent from "../../components/PokemonContent";
+import { PokemonGeneralInfo } from "../../components/PokemonGeneralInfo";
 
-interface ApiProps {
-  abilities: Array<{ ability: { name: string; url: string } }>;
-  base_experience: number;
-  forms: Array<{ name: string; url: string }>;
-  game_indices: Array<{ game_index: number; version: { name: string; url: string } }>;
-  height: number;
-  held_items: unknown;
-  id: number;
-  is_default: boolean;
-  location_area_encounters: string;
-  moves: Array<{ move: { name: string; url: string }; version_group_details: Array<string> }>;
-  name: string;
-  order: 1;
-  past_types: unknown;
-  species: { name: string; url: string };
-  sprites: {
-    back_default: string;
-    back_female: string;
-    back_shiny: string;
-    back_shiny_female: string;
-    front_default: string;
-    front_female: string;
-    front_shiny: string;
-    front_shiny_female: string;
-    other: {
-      dream_world: { front_default: string | null };
-      home: { front_default: string | null };
-      ["official-artwork"]: { front_default: string | null };
-    };
-  };
-  stats: Array<{ base_experience: number; effort: number; state: { name: string; url: string } }>;
-  types: Array<{ slot: number; type: { name: string; url: string } }>;
-  weight: number;
-}
-
+import { useGetPokemonInfo } from "../../service/http/GET/useGetPokemonInfo";
 function Pokemon() {
-  const { width } = useWindowDimensions();
-  const { info } = useContext(PokemonContext);
-  const [pokemonData, setPokemonData] = useState<ApiProps | null>(null);
-  const [imgPokemon, setImagePokemon] = useState<string>();
-  const [colors, setColors] = useState(["#e0ea77"]);
-  const [request, setRequest] = useState(false);
+  const [currentPokemonName, setCurrentPokemonName] = useState("bulbasaur");
 
-  useEffect(() => {
-    setRequest(false);
-    async function getData() {
-      api.get(`pokemon/${info.id}`).then((res) => setPokemonData(res.data));
-    }
-    if (info.name !== "" && info.id !== null) getData();
-  }, [info]);
+  const { data: pokemonInfo } = useGetPokemonInfo(currentPokemonName);
 
-  function getColors(array: Array<{ slot: number; type: { name: string; url: string } }>) {
-    let tmpColors: Array<string> = [];
-    array.forEach((obj: { type: { name: string } }) => {
-      let tmpColor = getColorPokemon(obj.type.name);
-      tmpColors.push(tmpColor);
-    });
-    setColors(tmpColors);
-  }
-  function checkImageIsAvaliable(data: ApiProps) {
-    if (data.sprites.other?.["official-artwork"].front_default != null) {
-      return setImagePokemon(data.sprites.other?.["official-artwork"].front_default);
-    } else if (data.sprites.other.dream_world.front_default != null) {
-      return setImagePokemon(data.sprites.other.dream_world.front_default);
-    } else {
-      return setImagePokemon(data.sprites.front_default);
-    }
-  }
-  useEffect(() => {
-    if (pokemonData !== null) {
-      checkImageIsAvaliable(pokemonData);
-      getColors(pokemonData.types);
-      setTimeout(() => setRequest(true), 1500);
-      console.log(pokemonData);
-    }
-  }, [pokemonData, info]);
+  console.log("currentPokemonInfo", pokemonInfo);
+  const color = getColorPokemon(pokemonInfo?.types[0].type.name ?? "normal");
 
   return (
-    <S.ContainerPokemon>
-      {width > 900 ? <SvgWave color={colors[0]} bg={"#fff"} /> : <></>}
-      {width <= 500 && pokemonData !== null ? (
-        <SvgWaveSmartphone color={colors[0]} bg={"transparent"} />
-      ) : (
-        <></>
-      )}
-      <main>
-        <SearchPokemon color={colors[0]} />
+    <S.View bgColor={color}>
+      <S.Container>
+        <div className="svg wave">
+          <SvgWave color={color} bg={"#fff"} />
+        </div>
 
-        {pokemonData !== null ? (
-          <PokemonContent
-            structure={[pokemonData.weight, pokemonData.height]}
-            abilities={pokemonData.abilities}
-            stats={pokemonData.stats}
-            request={request}
-            color={colors[0]}
+        {/* <div className="svg phone">
+          <SvgWaveSmartphone color={color} bg={"transparent"} />
+        </div> */}
+
+        <section>
+          <SearchPokemon
+            color={color}
+            pokemonName={currentPokemonName}
+            setPokemonName={setCurrentPokemonName}
           />
-        ) : (
-          <></>
-        )}
-      </main>
-      <S.GeneralInfo color={colors[0]} order={pokemonData !== null ? true : false}>
-        <article>
-          {pokemonData != null ? (
-            <>
-              <h2>{pokemonData.name}</h2>
-
-              <ul>
-                {pokemonData.types.map((obj: { type: { name: string } }, index: number) => (
-                  <S.TypeCard key={obj.type.name} color={`${colors[index]}`}>
-                    {obj.type.name}
-                  </S.TypeCard>
-                ))}
-              </ul>
-            </>
-          ) : (
-            <></>
-          )}
-        </article>
-        <aside>{pokemonData != null ? <img src={imgPokemon} /> : <></>}</aside>
-      </S.GeneralInfo>
-    </S.ContainerPokemon>
+          <PokemonGeneralInfo data={pokemonInfo} />
+        </section>
+      </S.Container>
+    </S.View>
   );
 }
 
